b5fa7e7a8d6d02ae25786cff4643d90647cc500b
Hunter Freyer <yt@hjfreyer.com>
2019-04-17 17:39:21 +0000

    Dropping.

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,62 @@ import * as state from './state';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
-const actions = new rx.Subject<state.Action>();
+const manualActions = new rx.Subject<state.Action>();
+const ticks = rx.timer(0, 1000 / 60).pipe(
+  rxop.map(_ => ({ kind: 'tick' } as state.Action))
+)
+
+const keyDowns =
+  rx.fromEvent(document, "keydown") as rx.Observable<KeyboardEvent>;
+const keyUps =
+  rx.fromEvent(document, "keyup") as rx.Observable<KeyboardEvent>;
+
+function keyToInput(key: string): state.InputType | null {
+  switch (key) {
+    case 'ArrowLeft':
+      return 'LEFT';
+    case 'ArrowRight':
+      return 'RIGHT';
+    case 'ArrowDown':
+      return 'DOWN';
+    default:
+      return null;
+  }
+}
+
+const keyPresses = rx.merge(keyUps, keyDowns).pipe(
+  // Ignore repeated keydowns unless separated by a keyup.
+  rxop.groupBy(e => e.key),
+  rxop.map(group => group.pipe(rxop.distinctUntilKeyChanged('type'))),
+  rxop.mergeAll(),
+
+  // Convert to a single stream of keys.
+  rxop.scan<KeyboardEvent, state.InputType>((acc, val) => {
+    const input = keyToInput(val.key);
+
+    // Unrecognized keys do nothing.
+    if (input == null) { return acc; }
+
+    // If we released a key...
+    if (val.type == 'keyup') {
+      // Cancel the input if it's the active key. Otherwise, ignore.
+      return input == acc ? 'NONE' : acc;
+    }
+    if (val.type != 'keydown') {
+      throw 'Unexpected key event';
+    }
+    return input;
+  }, 'NONE'),
+  rxop.distinctUntilChanged(),
+);
+
+keyPresses.subscribe(console.log);
+
+const inputActions = keyPresses.pipe(
+  rxop.map(input => ({ kind: 'input', input } as state.Action))
+);
+
+const actions = rx.merge(manualActions, inputActions, ticks);
 
 const states = actions.pipe(rxop.scan<state.Action, state.State>(state.apply, state.INITIAL), rxop.startWith(state.INITIAL));
 
@@ -19,7 +74,8 @@ const root = document.getElementById('root') as HTMLElement;
 
 doms.subscribe((d) => ReactDOM.render(d, root));
 
+
 import SHAPES from './shapes';
 window['SHAPES'] = SHAPES;
-window['DISPATCH'] = (a: state.Action) => actions.next(a);
+window['DISPATCH'] = (a: state.Action) => manualActions.next(a);
 registerServiceWorker();
--- src/state.ts
+++ src/state.ts
@@ -3,29 +3,35 @@ import produce from 'immer';
 import * as shape from './shape';
 import SHAPES from './shapes';
 
-export type Action = Tick | Translate;
+export type Action = Tick | Input;
 
+export type InputType = 'NONE' | 'LEFT' | 'RIGHT' | 'DOWN';
 type Tick = { kind: 'tick' };
-type Translate = {
-  kind: 'translate';
-  dRow: number;
-  dCol: number;
+type Input = {
+  kind: 'input'
+  input: InputType
 }
 
 export type State = {
-  width: number,
-  height: number,
-  board: number[][],
+  width: number
+  height: number
   activeShape: {
-    shapeIdx: number,
-    dRow: number,
-    dCol: number,
-  }
+    shapeIdx: number
+    dRow: number
+    dCol: number
+  },
+
+  input: InputType
+  dropDelay: number
+  board: number[][]
 };
 
 export const INITIAL: State = {
   width: 12,
   height: 24,
+  activeShape: { shapeIdx: 10, dRow: 0, dCol: 0 },
+  input: 'NONE',
+  dropDelay: 60,
   board: (() => {
     const board: number[][] = [];
     for (let i = 0; i < 24; i++) {
@@ -33,37 +39,64 @@ export const INITIAL: State = {
     }
     return board;
   })(),
-  activeShape: { shapeIdx: 10, dRow: 0, dCol: 0 },
 };
 
-function doTranslate(s: State, a: Translate): State {
+function attemptTranslateActive(s: State, dRow: number, dCol: number): boolean {
+  const [minRow, minCol, maxRow, maxCol] = shape.bbox(SHAPES[s.activeShape.shapeIdx]);
+  if (minRow + s.activeShape.dRow + dRow < 0) {
+    return false;
+  }
+  if (maxRow + s.activeShape.dRow + dRow >= s.height) {
+    return false;
+  }
+  if (minCol + s.activeShape.dCol + dCol < 0) {
+    return false;
+  }
+  if (maxCol + s.activeShape.dCol + dCol >= s.width) {
+    return false;
+  }
+
+  s.activeShape.dRow += dRow;
+  s.activeShape.dCol += dCol;
+
+  return true;
+}
+
+function doTick(s: State): State {
   return produce(s, s => {
-    const [minRow, minCol, maxRow, maxCol] = shape.bbox(SHAPES[s.activeShape.shapeIdx]);
-    if (minRow + s.activeShape.dRow + a.dRow < 0) {
-      return s;
+    if (s.dropDelay == 0) {
+      s.dropDelay = 60
+      attemptTranslateActive(s, 1, 0);
+    } else {
+      s.dropDelay--;
     }
-    if (maxRow + s.activeShape.dRow + a.dRow >= s.height) {
-      return s;
-    }
-    if (minCol + s.activeShape.dCol + a.dCol < 0) {
-      return s;
-    }
-    if (maxCol + s.activeShape.dCol + a.dCol >= s.width) {
-      return s;
-    }
+  });
+}
 
-    s.activeShape.dRow += a.dRow;
-    s.activeShape.dCol += a.dCol;
-
-    return s;
+function doInput(s: State, a: Input): State {
+  return produce(s, s => {
+    s.input = a.input;
+    switch (a.input) {
+      case 'NONE':
+        break;
+      case 'LEFT':
+        attemptTranslateActive(s, 0, -1);
+        break;
+      case 'DOWN':
+        attemptTranslateActive(s, 1, 0);
+        break;
+      case 'RIGHT':
+        attemptTranslateActive(s, 0, 1);
+        break;
+    }
   });
 }
 
 export function apply(s: State, a: Action): State {
   switch (a.kind) {
     case 'tick':
-      return s;
-    case 'translate':
-      return doTranslate(s, a);
+      return doTick(s);
+    case 'input':
+      return doInput(s, a);
   }
 }
