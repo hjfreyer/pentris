367c9162f25b1852e151e65bad333cd26d459439
Hunter Freyer <yt@hjfreyer.com>
2019-05-16 22:54:25 -0400

    Updates

--- v3/service-worker.js
+++ v3/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/v3/precache-manifest.99745324e24575c35ce65365285dc65c.js"
+  "/v3/precache-manifest.0cd5f2907e8012f09dd5b8e70204b332.js"
 );
 
 self.addEventListener('message', (event) => {
