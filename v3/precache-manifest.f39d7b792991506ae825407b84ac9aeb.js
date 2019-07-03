67f3cfa44d66845e6872d238677ee73d2a1bef44
Hunter Freyer <yt@hjfreyer.com>
2019-07-03 11:18:51 -0400

    Updates

---
+++ v3/precache-manifest.f39d7b792991506ae825407b84ac9aeb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0befd03b884e5a5e70ee4908c40881b7",
+    "url": "/v3/index.html"
+  },
+  {
+    "revision": "03fff5ad2e3aae41c20f",
+    "url": "/v3/static/css/main.e63e451a.chunk.css"
+  },
+  {
+    "revision": "992830247b575428e454",
+    "url": "/v3/static/js/2.be404c45.chunk.js"
+  },
+  {
+    "revision": "03fff5ad2e3aae41c20f",
+    "url": "/v3/static/js/main.697616c5.chunk.js"
+  },
+  {
+    "revision": "2233f0a171fd260d702f",
+    "url": "/v3/static/js/runtime~main.88f5b7f6.js"
+  }
+]);
--- v3/service-worker.js
+++ v3/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/v3/precache-manifest.45b37ff0ec8c3c2bad2efb66c775a12e.js"
+  "/v3/precache-manifest.f39d7b792991506ae825407b84ac9aeb.js"
 );
 
 self.addEventListener('message', (event) => {
